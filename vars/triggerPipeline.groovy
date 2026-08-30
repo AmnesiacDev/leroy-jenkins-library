13b84be750ae3dd6357628d325742911e4424b34
AmnesiacDev <46249408+AmnesiacDev@users.noreply.github.com>
2026-08-30 16:58:47 +0300

    Update triggerPipeline.groovy

--- vars/triggerPipeline.groovy
+++ vars/triggerPipeline.groovy
@@ -2,6 +2,6 @@ def call(String job_name){
     def result = build job: "${job_name}", wait: true, propagate: false
     echo "Backend pipeline (build/test/debug/deploy) result: ${result.result}"
     if (result.result != 'SUCCESS') {
-        unstable("Backend downstream pipeline did not succeed: ${result.result}")
+        unstable("Backend downstream pipeline did not succeed beepboop: ${result.result}")
     }
 }
