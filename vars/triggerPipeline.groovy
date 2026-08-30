a449ead63f5a780ec5f4766068bfba75c3df885f
AmnesiacDev <46249408+AmnesiacDev@users.noreply.github.com>
2026-08-30 16:53:16 +0300

    Update job name parameter in triggerPipeline function

--- vars/triggerPipeline.groovy
+++ vars/triggerPipeline.groovy
@@ -1,5 +1,5 @@
 def call(String job_name){
-    def result = build job: "${BACKEND_JOB_NAME}", wait: true, propagate: false
+    def result = build job: "${job_name}", wait: true, propagate: false
     echo "Backend pipeline (build/test/debug/deploy) result: ${result.result}"
     if (result.result != 'SUCCESS') {
         unstable("Backend downstream pipeline did not succeed: ${result.result}")
