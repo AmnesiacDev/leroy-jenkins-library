e6836224bea8aca0fd6cc04aec8afdc9feb5f92e
AmnesiacDev <46249408+AmnesiacDev@users.noreply.github.com>
2026-08-31 13:11:23 +0300

    Simplify composeDeploy function
    Removed service_name parameter and simplified docker commands.

--- vars/composeDeploy.groovy
+++ vars/composeDeploy.groovy
@@ -1,6 +1,5 @@
-def call(String service_name){
+def call(){
   sh """
-        docker compose build ${service_name}
-        docker compose up -d --no-deps ${service_name}
+        docker compose up -d 
     """
 }
