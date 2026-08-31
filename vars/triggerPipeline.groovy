71973cdbbe389344ce57cc0fa34c139c1f015a46
AmnesiacDev <46249408+AmnesiacDev@users.noreply.github.com>
2026-08-31 12:13:02 +0300

    Update triggerPipeline.groovy

--- vars/triggerPipeline.groovy
+++ vars/triggerPipeline.groovy
@@ -1,8 +1,21 @@
-def call(String job_name, String build_num){
-    def result = build job: job_name, wait: true, propagate: false, parameters: [string (name: IMAGE_TAG, value: build_num)]
-    echo "Backend pipeline (build/test/debug/deploy) result: ${result.result}"
+def call(String job_name, String build_num) {
+    def result = build(
+        job: job_name,
+        wait: true,
+        propagate: false,
+        parameters: [
+            string(
+                name: 'IMAGE_TAG',
+                value: build_num
+            )
+        ]
+    )
+
+    echo "Downstream pipeline ${job_name} result: ${result.result}"
+
     if (result.result != 'SUCCESS') {
-        unstable("Backend downstream pipeline did not succeed beepboop: ${result.result}")
+        unstable("Downstream pipeline ${job_name} did not succeed: ${result.result}")
     }
+
     return result
 }
