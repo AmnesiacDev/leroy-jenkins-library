ff5a74f44d5418d355bbb2a67477b47e48d7082b
AmnesiacDev <46249408+AmnesiacDev@users.noreply.github.com>
2026-08-31 12:08:34 +0300

    Refactor triggerPipeline to use direct parameters

--- vars/triggerPipeline.groovy
+++ vars/triggerPipeline.groovy
@@ -1,5 +1,5 @@
 def call(String job_name, String build_num){
-    def result = build job: "${job_name}", wait: true, propagate: false, parameters: [string (name: IMAGE_TAG, value: "${build_num}")]
+    def result = build job: job_name, wait: true, propagate: false, parameters: [string (name: IMAGE_TAG, value: build_num)]
     echo "Backend pipeline (build/test/debug/deploy) result: ${result.result}"
     if (result.result != 'SUCCESS') {
         unstable("Backend downstream pipeline did not succeed beepboop: ${result.result}")
