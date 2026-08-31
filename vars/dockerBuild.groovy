cb9220e06e446f6340b02c5ec91e08be15a94ff3
AmnesiacDev <46249408+AmnesiacDev@users.noreply.github.com>
2026-08-31 10:44:03 +0300

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,3 +1,7 @@
 def call (String image_name, String build_num){
-  sh "docker build -t ${image_name}:${build_num} ."
+  sh """
+    pwd
+    ls -la
+    docker build -t ${image_name}:${build_num} .
+  """
 }
