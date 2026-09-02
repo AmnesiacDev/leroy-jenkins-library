c73598fca784815fc1141088ca99578326fb73f4
AmnesiacDev <46249408+AmnesiacDev@users.noreply.github.com>
2026-09-02 14:21:17 +0300

    Fix error handling in composeBuild function

--- vars/composeBuild.groovy
+++ vars/composeBuild.groovy
@@ -1,7 +1,7 @@
 def call(String service_name){
   try{
     sh "docker compose build ${service_name}"
-  }else (err){
+  }catch (err){
     currentBuild.result = 'FAILURE'
     error("ERROR: failed at Build with -> ${err}")
   }
