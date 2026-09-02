e236a4590af0d981db408f0ac2b5a53052a4c713
AmnesiacDev <46249408+AmnesiacDev@users.noreply.github.com>
2026-09-02 14:21:04 +0300

    Update composeDeploy.groovy

--- vars/composeDeploy.groovy
+++ vars/composeDeploy.groovy
@@ -1,5 +1,10 @@
 def call(String service_name){
-  sh """
-        docker compose up -d "${service_name}"
-    """
+  try{
+        sh """
+              docker compose up -d "${service_name}"
+          """
+  }catch (err){
+        currentBuild.result = 'FAILURE'
+        error("ERROR: failed at Deploy with -> ${err}")
+  }
 }
