6f76fb5fb3ed1008c31519d901a755dc48b30cac
AmnesiacDev <46249408+AmnesiacDev@users.noreply.github.com>
2026-09-02 13:56:55 +0300

    Update error messages for directory checks

--- vars/isValidGitDir.groovy
+++ vars/isValidGitDir.groovy
@@ -3,15 +3,15 @@ def call(String repo_name){
       REPO_DIR="${repo_name}"
 
       if [ ! -d "\$REPO_DIR" ]; then
-          echo "ERROR: Test directory does not exist: \$REPO_DIR"
+          echo "ERROR: Directory does not exist: \$REPO_DIR"
           exit 1
 
       elif [ ! -d "\$REPO_DIR/.git" ]; then
-          echo "ERROR: Test directory exists but is not a Git repository: \$REPO_DIR"
+          echo "ERROR: Directory exists but is not a Git repository: \$REPO_DIR"
           exit 1
 
       else
-          echo "Test repository exists. Moving to Test stage."
+          echo "Repository exists. Moving to Test stage."
       fi
   """
 }
