9a60bb15d156502d74ad2bf7bf817ee7f99ce600
AmnesiacDev <46249408+AmnesiacDev@users.noreply.github.com>
2026-09-07 12:41:56 +0300

    Update composeDeploy.groovy

--- vars/composeDeploy.groovy
+++ vars/composeDeploy.groovy
@@ -1,10 +1,14 @@
-def call(String service_name){
-  try{
-        sh """
-              docker compose up -d "${service_name}"
-          """
-  }catch (err){
+def call(String service_name, String env_file = null) {
+    try {
+        if (env_file?.trim()) {
+            withEnv(["ENV_FILE=${env_file}"]) {
+                sh "docker compose up -d '${service_name}'"
+            }
+        } else {
+            sh "docker compose up -d '${service_name}'"
+        }
+    } catch (err) {
         currentBuild.result = 'FAILURE'
         error("ERROR: failed at Deploy with -> ${err}")
-  }
+    }
 }
