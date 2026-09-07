444f92d4d66467de254c66a346c6cb5f28165975
AmnesiacDev <46249408+AmnesiacDev@users.noreply.github.com>
2026-09-07 12:31:41 +0300

    Update composeBuild.groovy

--- vars/composeBuild.groovy
+++ vars/composeBuild.groovy
@@ -1,6 +1,10 @@
-def call(String service_name){
+def call(String service_name, String env_file = null){
   try{
-    sh "docker compose build ${service_name}"
+    if (env_file) {
+            sh "docker compose --env-file '${env_file}' build ${service_name}"
+        } else {
+            sh "docker compose build ${service_name}"
+        }
   }catch (err){
     currentBuild.result = 'FAILURE'
     error("ERROR: failed at Build with -> ${err}")
