73c91994db10349a48da86a542320dd51f559065
AmnesiacDev <46249408+AmnesiacDev@users.noreply.github.com>
2026-09-07 17:33:09 +0300

    Refactor call function for Docker compose build

--- vars/composeBuild.groovy
+++ vars/composeBuild.groovy
@@ -1,12 +1,12 @@
-def call(String service_name, String env_file = null){
-  try{
-    if (env_file) {
-            sh "docker compose --env-file '${env_file}' build ${service_name}"
+def call(String service_name, String env_file = null) {
+    try {
+        if (env_file?.trim()) {
+            sh "docker compose --env-file '${env_file}' build '${service_name}'"
         } else {
-            sh "docker compose build ${service_name}"
+            sh "docker compose build '${service_name}'"
         }
-  }catch (err){
-    currentBuild.result = 'FAILURE'
-    error("ERROR: failed at Build with -> ${err}")
-  }
+    } catch (err) {
+        currentBuild.result = 'FAILURE'
+        error("ERROR: failed at Build with -> ${err}")
+    }
 }
